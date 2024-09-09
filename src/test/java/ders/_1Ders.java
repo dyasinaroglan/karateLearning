66349f7239704e52ab7e9f878df08117ac77c3c0
Yasin Aroglan <yasinargln264@gmail.com>
2024-09-09 21:13:08 +0300

    karate repeat

---
+++ src/test/java/ders/_1Ders.java
@@ -0,0 +1,50 @@
+package ders;
+
+public class _1Ders {
+
+    /*
+    Karate'nin Avantajları:
+
+
+BDD (Behavior-Driven Development) Syntax: Karate, Cucumber tabanlı bir BDD senaryosuna sahiptir ve Gherkin dilini kullanır.
+Bu, daha az teknik olan kişiler için bile testlerin yazılmasını kolaylaştırır.
+Tümleşik JSON ve XML Desteği: JSON ve XML üzerinde veri işlemek için Karate, yerleşik destek sunar.
+Veri doğrulama işlemleri çok daha basit ve anlaşılırdır.
+Gelişmiş Assertion Desteği: Karate, "deep equals" ve "contains" gibi JSON ve XML için güçlü doğrulama yöntemlerine sahiptir.
+Tek adımda tüm JSON içeriğini doğrulamak mümkündür.
+Test-Scripting Desteği: Karate, JavaScript desteği ile testlerde ileri seviye esneklik sağlar.
+Özelleştirilmiş işlevler ve test senaryoları yazılabilir.
+Plug-and-Play HTTP Client: Karate'nin HTTP istemcisi çok esnektir ve kolayca değiştirilip kullanılabilir.
+IDE Desteği: Karate'nin Visual Studio Code ve IntelliJ gibi popüler IDE'ler için debug ve step-through desteği vardır, böylece adım adım test takibi yapılabilir.
+
+
+     Karate'nin Dezavantajları:
+
+
+Java API Kısıtlılığı: Karate'nin Java API desteği REST-assured kadar geniş değildir.
+IDE Destek Sorunları: REST-assured kadar zengin bir IDE entegrasyonu ve refactoring desteği yoktur.
+Yaygın Olmayan Kaynaklar: Karate, REST-assured kadar uzun süredir piyasada olmadığı için topluluk ve kaynak desteği daha az olabilir.
+
+
+
+      REST-assured'in Avantajları:
+
+
+Zengin Java Entegrasyonu: REST-assured, doğrudan Java ile entegre çalışır. Java geliştiricileri için oldukça tanıdık bir yapıdadır.
+Olgun ve Yaygın Kullanım: REST-assured daha eski ve daha olgun bir kütüphanedir. Daha fazla kaynak, blog yazısı ve StackOverflow soruları bulunur.
+Detaylı IDE Desteği: IntelliJ ve Eclipse gibi IDE'ler ile daha güçlü entegrasyona sahiptir. Otomatik tamamlama ve refactoring gibi özellikleri destekler.
+XML Destek Seçenekleri: REST-assured, XMLSlurper ve GroovyPath gibi farklı seçeneklerle XML veri işleme desteği sunar.
+
+
+      REST-assured'in Dezavantajları:
+
+
+Daha Karmaşık Yazım: REST-assured, Karate'nin aksine daha fazla teknik bilgi gerektirir.
+Özellikle Fluent Interface kullanımı ve DSL (Domain Specific Language) yapısı, kullanıcıları zorlayabilir.
+JSON ve XML Veri İşlemede Eksiklikler: REST-assured'de JSON ve XML veri doğrulama Karate kadar kolay değildir.
+Özellikle derin veri doğrulama işlemleri için ek kütüphaneler gerekebilir.
+Zorlayıcı Kurulum: REST-assured'in bağımlılık yönetimi Karate'ye göre daha karmaşık olabilir.
+Apache HTTP bileşenleri gibi bağımlılıklar eski API'lere dayanır ve güncellenmesi gerekebilir.
+
+     */
+}
---
+++ src/test/java/ders/_2Ders.java
@@ -0,0 +1,47 @@
+package ders;
+
+public class _2Ders {
+
+
+    /*
+    Json nedir?
+
+
+    Objeler ({}): key-value çiftlerinden oluşur.
+    Her anahtar bir string olmalıdır ve değer ise herhangi bir veri tipi olabilir (string, number, array, boolean, null)
+
+       {
+         "name": "John",
+         "age": 30,
+         "city": "New York"
+       }
+
+
+      Diziler ([]): Bir dizi (array), köşeli parantez içinde sıralı değerlerin bir listesidir.
+      Bu değerler, farklı veri tiplerine sahip olabilir (string, number, object, array, boolean, null).
+
+      ["apple", "banana", "cherry"]
+
+
+      İç İçe Objeler ve Diziler:
+
+      {
+       "name": "Bob",
+       "address": {
+       "city": "San Francisco",
+       "state": "CA"
+      },
+       "hobbies": ["music", "travel", {"type": "sport", "name": "tennis"}]
+     }
+
+
+                Veri Tipleri
+             String: Çift tırnaklar arasındaki metin. Örnek: "hello"
+             Number: Tam sayılar veya ondalıklı sayılar. Örnek: 42, 3.14
+             Boolean: true veya false değerleri.
+             null: Boş veya tanımsız bir değeri belirtir.
+
+
+
+     */
+}
