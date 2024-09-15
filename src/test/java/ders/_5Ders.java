183d07280687d94658dc1f769679051120b8995a
Yasin Aroglan <yasinargln264@gmail.com>
2024-09-15 23:24:25 +0300

    karate setupwe

---
+++ src/test/java/ders/_5Ders.java
@@ -0,0 +1,37 @@
+package ders;
+
+public class _5Ders {
+
+
+    /*
+
+    <dependency>
+      <groupId>com.intuit.karate</groupId>
+      <artifactId>karate-junit5</artifactId>
+      <version>${karate.version}</version>
+      <scope>test</scope>
+    </dependency>
+
+
+    <karate.version>1.0.0</karate.version>
+
+    Karate içib iki bağımlılığımız var.
+
+
+    2 tane de eklentimiz (plugin) var.
+
+    <plugin>
+        <groupId>org.apache.maven.plugins</groupId>
+        <artifactId>maven-compiler-plugin</artifactId>
+        <version>${maven.compiler.version}</version>
+        <configuration>
+          <encoding>UTF-8</encoding>
+          <source>${java.version}</source>
+          <target>${java.version}</target>
+          <compilerArgument>-Werror</compilerArgument>
+        </configuration>
+      </plugin>
+
+      Java klasörünün içinde 2 önemli dosya var. logback-test.xml ve karate-config.js
+     */
+}
